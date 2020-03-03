d50ac0c9ad86565739aad0d5d3a7207b8c44c4b0
cficht <chris.ficht@gmail.com>
2020-03-03 14:59:30 -0800

    2 spaces again

--- server.js
+++ server.js
@@ -129,5 +129,5 @@ app.get('*', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log('server running on PORT', PORT);
+    console.log('server running on PORT!', PORT);
 });
